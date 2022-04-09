901f692397697306031e778e995a16fb3b9209d7
Grigore Stefan <g_stefan@yahoo.com>
2022-04-09 21:06:12 +0300

    Update

--- build/msvc.vendor.cmd
+++ build/msvc.vendor.cmd
@@ -23,11 +23,11 @@ del /F /Q archive\%PROJECT%-%VERSION%.7z goto:eof
 rem Source
 pushd archive
 set VENDOR=%PROJECT%-%VERSION%
-set WEB_LINK=https://www.cpan.org/src/5.0/perl-5.34.0.tar.gz
+set WEB_LINK=https://www.cpan.org/src/5.0/perl-5.34.1.tar.gz
 if not exist %VENDOR%.tar.gz curl --insecure --location %WEB_LINK% --output %VENDOR%.tar.gz
 7z x %VENDOR%.tar.gz -so | 7z x -aoa -si -ttar -o.
 del /F /Q %VENDOR%.tar.gz
 if exist %VENDOR%.7z del /F /Q %VENDOR%.7z
-7zr a -mx9 -mmt4 -r- -sse -w. -y -t7z %VENDOR%.7z %VENDOR%
+7z a -mx9 -mmt4 -r- -sse -w. -y -t7z %VENDOR%.7z %VENDOR%
 rmdir /Q /S %VENDOR%
 popd
